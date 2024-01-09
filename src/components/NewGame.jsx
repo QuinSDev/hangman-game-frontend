e8f246208b46d41e3d01e474df28d08a8b4ee248
QuinSDev <quindev97@gmail.com>
2024-01-09 16:13:46 -0500

    Se agrega cambios al componente como linkear el botón hacia atrás, se agrega componentes como HangamanImage y Board

--- src/components/NewGame.jsx
+++ src/components/NewGame.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logoHangman from "../assets/images/logo_hangman.png";
 import bgWindowContent from "../assets/images/textura-papel-cuadriculado.jpg";
+import { HangmanImage } from "./HangmanImage";
+import { ImagesHangman } from "../api/ImagesHangman";
+import { Link } from "react-router-dom";
+import { Board } from "./Board";
 
-export const NewGame = () => {
+export const NewGame = ({
+  position,
+  wordSecret,
+  onEnter,
+  scoreGame,
+  error,
+}) => {
   const borderStyle = {
     borderLeft: "3px solid #4c4a48",
     borderRight: "3px solid #4c4a48",
@@ -11,7 +21,7 @@ export const NewGame = () => {
 
   const cardStyle = {
     width: "750px",
-  }
+  };
 
   const bodyStyle = {
     backgroundImage: `url(${bgWindowContent})`,
@@ -28,7 +38,7 @@ export const NewGame = () => {
   };
 
   return (
-    <div className="card shadow-xl rounded-md" style={cardStyle}>
+    <div className="card shadow-xl rounded-md flex flex-col" style={cardStyle}>
       <div className="h-12 bg-[#4c4a48] flex justify-start items-center rounded-t-md px-2">
         <img className="w-6 h-6" src={logoHangman} alt="" />
         <h3 className="ml-5 font-semibold text-base text-white">
@@ -36,16 +46,33 @@ export const NewGame = () => {
         </h3>
       </div>
       <div className="rounded-b-md h-full w-full" style={borderStyle}>
-        <div className="rounded-b-md h-full" style={bodyStyle}>
-            <div className="flex gap-3 p-2">
-                <button className="w-24 h-8 text-black text-xs text-center " style={bgButton}>Nuevo Juego</button>
-                <button className="w-24 h-8 text-black text-xs text-center" style={bgButton}>Atrás
+        <div
+          className="rounded-b-md h-full relative overflow-hidden"
+          style={bodyStyle}
+        >
+          <div className="flex gap-3 p-2">
+            <button
+              className="w-24 h-8 text-black text-xs text-center "
+              style={bgButton}
+            >
+              Nuevo Juego
             </button>
-            </div>
-            <div id="hangman" className="py-1 px-4">
-
-            </div>
-            <div></div>
+            <Link to="/">
+              <button
+                className="w-24 h-8 text-black text-xs text-center"
+                style={bgButton}
+              >
+                Atrás
+              </button>
+            </Link>
+          </div>
+          <HangmanImage imagesHangman={ImagesHangman} position={position} />
+          <Board
+            wordSecret={wordSecret}
+            onEnter={onEnter}
+            scoreGame={scoreGame}
+            error={error}
+          />
         </div>
       </div>
     </div>
