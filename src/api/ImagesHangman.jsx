82d7c3918e9e7e471c65eeb4c97508ea14bfb84b
QuinSDev <quindev97@gmail.com>
2024-01-09 16:23:13 -0500

    Se crea las APIs de las imagenes del ahorcado y también de las palabras de manera local, para que sea más rápido y fluido la carga

---
+++ src/api/ImagesHangman.jsx
@@ -0,0 +1,17 @@
+import estructura from "../assets/images/estructura-min.png"
+import head from "../assets/images/head-min.png"
+import body from"../assets/images/body-min.png"
+import handI from"../assets/images/hand-min.png"
+import handD from"../assets/images/hand2-min.png"
+import piernaI from "../assets/images/pierna-min.png"
+import piernaD from "../assets/images/pierna2-min.png"
+
+export const ImagesHangman = [
+    estructura,
+    head,
+    body,
+    handI,
+    handD,
+    piernaI,
+    piernaD
+]
