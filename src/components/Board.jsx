4531d2e8bb28df4ba34baca580faa4d0c3088804
QuinSDev <quindev97@gmail.com>
2024-01-09 16:21:48 -0500

    Este componente rendizará los componentes para ingresar una letra y también el que mostrará la palabra con l puntaje. Además mostrará un mensaje si el usuario no ingresa o ingresa mas de 1 letra en el input.

---
+++ src/components/Board.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { LetterInput } from "./LetterInput";
+import { WordDisplay } from "./WordDisplay";
+
+export const Board = ({ wordSecret, scoreGame, onEnter, error }) => {
+  return (
+    <div className="relative flex flex-col  h-44 px-8 py-7">
+      <LetterInput onEnter={onEnter} />
+      <div className="mt-1 flex h-4">
+        <div className="w-44"></div>
+        <div className="ml-2 text-red-700 text-sm">{error}</div>
+      </div>
+      <WordDisplay wordSecret={wordSecret} scoreGame={scoreGame} />
+    </div>
+  );
+};
