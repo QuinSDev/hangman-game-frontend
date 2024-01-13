57be273eb2982f8f51ee7f6acad00a5348e78163
QuinSDev <quindev97@gmail.com>
2024-01-13 12:28:48 -0500

    Se ajusta el Board para que se vea mejor en movil

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,10 +4,10 @@ import { WordDisplay } from "./WordDisplay";
 
 export const Board = ({ wordSecret, scoreGame, onEnter, error, inputValue, setInputValue }) => {
   return (
-    <div className="relative flex flex-col  h-44 px-8 py-7">
+    <div className="relative flex flex-col  h-44 px-4 sm:px-8 py-7">
       <LetterInput onEnter={onEnter} inputValue={inputValue} setInputValue={setInputValue} />
       <div className="mt-1 flex h-4">
-        <div className="w-44"></div>
+        <div className="w-24 sm:w-44"></div>
         <div className="ml-2 text-red-700 text-sm">{error}</div>
       </div>
       <WordDisplay wordSecret={wordSecret} scoreGame={scoreGame} />
--- src/components/WordDisplay.jsx
+++ src/components/WordDisplay.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export const WordDisplay = ({ wordSecret, scoreGame }) => {
   return (
-    <div className='mt-10 flex w-full h-7 items-center gap-5'>
-        <label htmlFor="" className='text-black'>PALABRA:</label>
-        <p className='text-black'>{wordSecret}</p>
-        <div className='flex ml-auto'>
-            <label htmlFor="" className='text-black'>PUNTAJE:</label>
-            <p className='ml-3 w-9 text-black'>{scoreGame}</p>
-        </div>
+    <div className="mt-10 flex flex-col sm:flex-row w-full h-7 sm:items-center gap-5">
+      <div className="w-full flex gap-5">
+        <label htmlFor="" className="text-black">
+          PALABRA:
+        </label>
+        <p className="text-black">{wordSecret}</p>
+      </div>
+      <div className="hidden sm:flex ml-auto">
+        <label htmlFor="" className="text-black">
+          PUNTAJE:
+        </label>
+        <p className="ml-3 w-9 text-black">{scoreGame}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
